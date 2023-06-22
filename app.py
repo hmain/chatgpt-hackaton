28953891328168a1142abaf18cd196e2e419961d
AhmedLuay87 <130970238+AhmedLuay87@users.noreply.github.com>
2023-06-22 11:05:51 +0200

    Update app.py
    added aviation industry to the prompt

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def chat():
             },
             {
                 "role": "assistant",
-                "content": "I want you to create a professional CV in html format and using the below input:",
+                "content": "I want you to create a professional CV, for a consultant in the aviation area, in html format and using the below input:",
             },
             {"role": "assistant", "content": cv_input},
             {
